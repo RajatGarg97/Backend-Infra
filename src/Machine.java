b5eb48fe78a14875f7a796f04817c9bbb44be05d
RajatGarg97 <rg51096@gmail.com>
2020-06-15 22:08:28 +0530

    fixed managing admin control request

--- src/Machine.java
+++ src/Machine.java
@@ -36,14 +36,21 @@ public class Machine {
 		return this.isActive;
 	}
 
-	public void machine_down(String url) {
+	public void machine_down() {
 		this.isActive = false;
-//		this.logger(url);
+		this.logger("----MACHINE DOWN----");
 	}
+
+//	public void request(String url) {
+//		if (url.contains("down")) {
+//			this.machine_down();
+//		} else if (url.contains("up")) {
+//			
+//		}
+//	}
 
 	public void logger(String log) {
 		this.logFile.add(log);
-
 	}
 
 	public void printLogs() {
