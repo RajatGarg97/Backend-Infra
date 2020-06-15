56cb23108200681c3e1d75d9a10d4ea1e18b1edf
RajatGarg97 <rg51096@gmail.com>
2020-06-15 22:09:35 +0530

    separated admin control requests and added constructor

--- src/reverseProxy.java
+++ src/reverseProxy.java
@@ -4,54 +4,58 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class reverseProxy {
-		public String proxyURL;
-		public Queue<Machine> activeMachines;
-		public HashMap<String, Machine> machines;
-		public String[] IPlist;
+	public String proxyURL;
+	public Queue<Machine> activeMachines;
+	public Queue<Machine> readyMachines;
+	public HashMap<String, Machine> machines;
+	public String[] IPlist;
 
-		reverseProxy(String proxyURL, String[] IPlist) {
-			this.proxyURL = proxyURL;
-			this.activeMachines = new LinkedList<Machine>();
-			this.machines = new HashMap<String, Machine>();
-			this.IPlist = IPlist;
-		}
+	reverseProxy(String proxyURL, String[] IPlist) {
+		this.proxyURL = proxyURL;
+		this.activeMachines = new LinkedList<Machine>();
+		this.machines = new HashMap<String, Machine>();
+		this.IPlist = IPlist;
+		this.readyMachines = new LinkedList<Machine>();
+	}
 
-		public String getURL() {
-			return this.proxyURL;
-		}
+	public String getProxyURL() {
+		return this.proxyURL;
+	}
 
-		public void construct(ArrayList<Machine> machineList) {
-			for (int i = 0; i < machineList.size(); i++) {
-				Machine m = machineList.get(i);
-				this.machines.put(m.getIP(), m);
-				this.activeMachines.add(m);
-			}
-		}
-
-		public void request(String url) {
-			String[] urlarr = url.split("=", 2);
-
-			if (url.contains("machine_down")) {
-				this.machines.get(urlarr[1]).machine_down(url);
-			} else if (url.contains("machine_up")) {
-//				this.machines.get(urlarr[1]).logger(url);
-				ArrayList<String> inactiveMcLog = this.machines.get(urlarr[1]).getLogs();
-				this.machines.put(urlarr[1], new Machine(urlarr[1], inactiveMcLog));
-				this.activeMachines.add(this.machines.get(urlarr[1]));
-			} else {
-				while ((!this.activeMachines.isEmpty()) && (!this.activeMachines.peek().status())) {
-					this.activeMachines.poll();
-				}
-				Machine m = this.activeMachines.poll();
-				m.logger(url);
-				this.activeMachines.add(m);
-			}
-		}
-
-		public void generateLogs() {
-
-			for (String ip : this.IPlist) {
-				this.machines.get(ip).printLogs();
-			}
+	public void construct(ArrayList<Machine> machineList) {
+		for (int i = 0; i < machineList.size(); i++) {
+			Machine m = machineList.get(i);
+			this.machines.put(m.getIP(), m);
+			this.activeMachines.add(m);
 		}
 	}
+
+	public void specialRequest(String url) {
+		String[] urlarr = url.split("=", 2);
+		if (url.contains("down")) {
+			this.machines.get(urlarr[1]).machine_down();
+		} else {
+			ArrayList<String> inactiveMcLog = this.machines.get(urlarr[1]).getLogs();
+			inactiveMcLog.add("----MACHINE UP----");
+			this.machines.put(urlarr[1], new Machine(urlarr[1], inactiveMcLog));
+			this.readyMachines.add(this.machines.get(urlarr[1]));
+		}
+	}
+
+	public void request(String url) {
+		if (url.contains("machine_down") || url.contains("machine_up")) {
+			this.specialRequest(url);
+		} else {
+			while ((!this.activeMachines.isEmpty()) && (!this.activeMachines.peek().status())) {
+				this.activeMachines.poll();
+			}
+			while (!this.readyMachines.isEmpty()) {
+				this.readyMachines.poll();
+			}
+			Machine m = this.activeMachines.poll();
+			m.logger(url);
+			this.activeMachines.add(m);
+		}
+	}
+
+}
