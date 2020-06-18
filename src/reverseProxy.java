38d0a0b57fee3fad88f4991482d0e89fa16ef622
RajatGarg97 <rg51096@gmail.com>
2020-06-18 18:57:04 +0530

    Added Deque for LB and persistent Memory for machines

--- src/reverseProxy.java
+++ src/reverseProxy.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class reverseProxy {
 	public String proxyURL;
-	public Queue<Machine> activeMachines;
-	public Queue<Machine> readyMachines;
+	public Deque<Machine> activeMachines;
 	public HashMap<String, Machine> machines;
+	public HashMap<String, ArrayList<String>> persistentMem;
 	public String[] IPlist;
 
 	reverseProxy(String proxyURL, String[] IPlist) {
@@ -15,7 +15,7 @@ public class reverseProxy {
 		this.activeMachines = new LinkedList<Machine>();
 		this.machines = new HashMap<String, Machine>();
 		this.IPlist = IPlist;
-		this.readyMachines = new LinkedList<Machine>();
+		this.persistentMem = new HashMap<String, ArrayList<String>>();
 	}
 
 	public String getProxyURL() {
@@ -35,10 +35,8 @@ public class reverseProxy {
 		if (url.contains("down")) {
 			this.machines.get(urlarr[1]).machine_down();
 		} else {
-			ArrayList<String> inactiveMcLog = this.machines.get(urlarr[1]).getLogs();
-			inactiveMcLog.add("----MACHINE UP----");
-			this.machines.put(urlarr[1], new Machine(urlarr[1], inactiveMcLog));
-			this.readyMachines.add(this.machines.get(urlarr[1]));
+			this.machines.put(urlarr[1], new Machine(urlarr[1], this.persistentMem.get(urlarr[1])));
+			this.activeMachines.addFirst(this.machines.get(urlarr[1]));
 		}
 	}
 
@@ -46,16 +44,18 @@ public class reverseProxy {
 		if (url.contains("machine_down") || url.contains("machine_up")) {
 			this.specialRequest(url);
 		} else {
-			while ((!this.activeMachines.isEmpty()) && (!this.activeMachines.peek().status())) {
-				this.activeMachines.poll();
+			
+			while((!this.activeMachines.isEmpty()) && (!this.activeMachines.peek().status())) {
+				this.activeMachines.remove();
 			}
-			while (!this.readyMachines.isEmpty()) {
-				this.readyMachines.poll();
+			if(this.activeMachines.isEmpty()) {
+				System.out.println("[ERR!]: No Machine Available");
+				return;
 			}
 			Machine m = this.activeMachines.poll();
 			m.logger(url);
+			persistentMem.put(m.getIP(), m.getLogs());
 			this.activeMachines.add(m);
 		}
 	}
-
 }
