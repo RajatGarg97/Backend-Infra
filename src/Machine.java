3485dbcd6e460c56e9b65d44e010b427efa623ba
RajatGarg97 <rg51096@gmail.com>
2020-10-24 21:02:40 +0530

    Fixed comments and logging

--- src/Machine.java
+++ src/Machine.java
@@ -21,7 +21,7 @@ public class Machine {
 	}
 
 	/**
-	 * This is a constructor method.
+	 * This is another constructor method.
 	 * 
 	 * @param ip: It is the IP address of the machine object.
 	 * @param logFile: An ArrayList in which logs of the machine object are stored.
@@ -57,19 +57,20 @@ public class Machine {
 
 	/**
 	 * This method is used to switch status of machine object to active.
+	 *201
 	 */
 	public void machine_up() {
-		String str = "[INFO]: " + this.getIP() + " is up and running";
+		String str = "[INFO]: **** " + this.getIP() + " is up and running ****";
 		this.logger(str);
 		this.isActive = true;
 	}
 
 	/**
-	 * @param log: A string containing log request to be saved in logfile.
+	 * @param log: A string containing log request to be saved in log file.
 	 */
 	public void logger(String log) {
 		if (this.status()) {
-			String logstr = "[INFO]: " + log + " [response: 200]";
+			String logstr = "[INFO]: " + log + " [Status Code: 200]";
 			this.logFile.add(logstr);
 		} else {
 			this.logFile.add(log);
